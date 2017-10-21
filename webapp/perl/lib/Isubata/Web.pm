5122ecd6c1f0268bd8819e64c4882561bc3e94e9
silvers <ofsilvers+github@gmail.com>
2017-10-21 19:53:47 +0900

    もうmysqlは暇してるのでがんばらなくてよい

--- webapp/perl/lib/Isubata/Web.pm
+++ webapp/perl/lib/Isubata/Web.pm
@@ -253,36 +253,7 @@ get '/message' => sub {
         $last_message_id, $channel_id,
     );
 
-#     my $rows = $self->dbh->select_all(
-#         qq{
-# SELECT
-# message.id as message_id,
-# channel_id,
-# user_id,
-# user.name as user_name,
-# display_name,
-# avatar_icon,
-# content,
-# message.created_at
-# FROM message inner join user on user.id = user_id
-# WHERE message.id > ? AND channel_id = ? ORDER BY message.id DESC LIMIT 100
-# },
-#         $last_message_id, $channel_id,
-#     );
-
     my @res;
-    # for my $row (@$rows) {
-    #     unshift @res, {
-    #         id      => $row->{id},
-    #         user    => {
-    #             name => $row->{user_name},
-    #             display_name => $row->{display_name},
-    #             avatar_icon => $row->{avatar_icon},
-    #         },
-    #         date    => DateTime::Format::MySQL->parse_datetime($row->{created_at})->strftime("%Y/%m/%d %H:%M:%S"),
-    #         content => $row->{content},
-    #     };
-    # }
 
     if (0 < scalar @$rows) {
         my %users_map = map { $_->{id} => $_ } @{ $self->dbh->select_all(qq{SELECT id, name, display_name, avatar_icon FROM user WHERE id IN (?)}, [map $_->{user_id}, @$rows]) };
