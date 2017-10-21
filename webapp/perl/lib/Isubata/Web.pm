5e33ad5cb49ce32c4307d89a0fe48aa235bcc1c7
ar_tama <arata.makoto@gmail.com>
2017-10-21 15:49:55 +0900

    Fix filename

--- webapp/perl/lib/Isubata/Web.pm
+++ webapp/perl/lib/Isubata/Web.pm
@@ -435,7 +435,7 @@ post '/profile' => [qw/login_required/] => sub {
         $avatar_name = $digest . $ext;
         $avatar_data = $data;
 
-        system('cp ' . $file->path . ' ../public/icons/');
+        system('cp ' . $file->path . ' /home/isucon/isubata/webapp/public/icons/' . $avatar_name);
     }
 
     my $avatar_updated = ($avatar_name && $avatar_data);
